3686e2fb5a1faf6c9f8f43e2b011d3d7386ee975
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2016-02-16 21:15:57 +0100

    Initial commit for the Unreal Engine 4 C++ 2D Tutorial

---
+++ Source/UE4Tanks/UE4TanksGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4Tanks.h"
+#include "UE4TanksGameMode.h"
+
+
+
+
---
+++ Source/UE4Tanks/UE4TanksGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "UE4TanksGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4TANKS_API AUE4TanksGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
