e4f9da94bc313d38b484c4f4e6e852eb93128cdf
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2016-02-17 22:23:29 +0100

    Add a Tank Pawn

---
+++ Source/UE4Tanks/Tank.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4Tanks.h"
+#include "Tank.h"
+#include "PaperSpriteComponent.h"
+
+
+// Sets default values
+ATank::ATank()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("TankBase"));
+
+	TankDirection = CreateDefaultSubobject<UArrowComponent>(TEXT("TankDirection"));
+	TankDirection->AttachTo(RootComponent);
+
+	TankSprite = CreateDefaultSubobject<UPaperSpriteComponent>(TEXT("TankSprite"));
+	TankSprite->AttachTo(TankDirection);
+}
+
+// Called when the game starts or when spawned
+void ATank::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATank::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void ATank::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+}
+
---
+++ Source/UE4Tanks/Tank.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Pawn.h"
+#include "Tank.generated.h"
+
+UCLASS()
+class UE4TANKS_API ATank : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ATank();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+
+private:
+	// Helpful debug tool - which way is the tank facing?
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Tank", meta = (AllowPrivateAccess = "true"))
+	class UArrowComponent* TankDirection;
+
+	// Sprite for the tank body.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Tank", meta = (AllowPrivateAccess = "true"))
+	class UPaperSpriteComponent* TankSprite;
+};
